9916ee8e67217acfc4fb125707ae3f68e3c54ab9
Henri Sota <sotahenry@gmail.com>
2024-12-31 10:40:43 +0100

    package wxt

--- devenv.nix
+++ devenv.nix
@@ -4,8 +4,72 @@
   ...
 }: let
   inherit (lib) getExe;
+
+  wxt = let
+    pname = "wxt";
+    version = "v0.19.23";
+  in
+    with pkgs;
+      stdenv.mkDerivation (final: {
+        inherit pname version;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "wxt-dev";
+          repo = pname;
+          rev = "wxt-${version}";
+          sha256 = "sha256-qr4ON2EPU00rVHF7OgWL2WxRaIaWHbYHpj+HlQ/2600=";
+        };
+
+        pnpmDeps = pnpm_9.fetchDeps {
+          inherit (final) pname version src;
+          hash = "sha256-pOq1ftXVriFMopU6Ypg8aRV9sGJUl9jVQjsy0c3j+so=";
+        };
+
+        nativeBuildInputs = [
+          makeWrapper
+          nodejs_22
+          pnpm_9.configHook
+        ];
+
+        buildInputs = with pkgs; [
+          nodejs_22
+        ];
+
+        buildPhase = ''
+          runHook preBuild
+
+          pnpm --filter=wxt build
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p "$out/bin" "$out/lib/node_modules/wxt"
+          cp -r packages node_modules "$out/lib/node_modules/wxt"
+
+          makeWrapper "${lib.getExe nodejs_22}" "$out/bin/wxt" \
+            --inherit-argv0 \
+            --prefix NODE_PATH : $out/lib/node_modules/wxt/node_modules \
+            --prefix NODE_PATH : $out/lib/node_modules/wxt/packages/wxt/node_modules \
+            --add-flags "$out/lib/node_modules/wxt/packages/wxt/bin/wxt.mjs"
+
+          runHook postInstall
+        '';
+
+        meta = {
+          description = "Next-gen Web Extension Framework";
+          homepage = "https://github.com/wxt-dev/wxt";
+          license = lib.licenses.mit;
+          mainProgram = pname;
+        };
+      });
 in {
-  packages = [pkgs.web-ext];
+  packages = [
+    pkgs.web-ext
+    wxt
+  ];
 
   languages.javascript.enable = true;
 
