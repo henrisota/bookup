2184d8082cdafe121a782e1b826be2979543a2af
Henri Sota <sotahenry@gmail.com>
2025-01-05 17:38:24 +0100

    create initial design

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,3 +1,7 @@
+import logo from '@/assets/logo.svg';
+
+document.querySelector<HTMLImageElement>('#logo')!.src = logo;
+
 document.getElementById('bookUp')?.addEventListener('click', () => {
   browser.runtime.sendMessage({command: 'bookUp'});
 });
