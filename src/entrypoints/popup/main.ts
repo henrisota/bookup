739767d9adabd56209bf840c430cf10020cdf1b4
Henri Sota <sotahenry@gmail.com>
2025-01-05 17:38:35 +0100

    support dark mode

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -5,3 +5,8 @@ document.querySelector<HTMLImageElement>('#logo')!.src = logo;
 document.getElementById('bookUp')?.addEventListener('click', () => {
   browser.runtime.sendMessage({command: 'bookUp'});
 });
+
+document.documentElement.classList.toggle(
+  'dark',
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+);
