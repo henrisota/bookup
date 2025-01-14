4f27d4e63151c0ed9d0b47956d9d9d5392e8c6ed
Henri Sota <sotahenry@gmail.com>
2025-01-14 22:32:26 +0100

    configure root directory on demand via popup

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -6,8 +6,25 @@ async function main() {
   const configuration = new Configuration();
   const extensionConfiguration = await configuration.get();
 
+  const rootDirectory = document.querySelector<HTMLInputElement>('#rootDirectory');
+  const setRootDirectoryButton = document.getElementById('setRootDirectoryButton');
+
   document.querySelector<HTMLImageElement>('#logo')!.src = logo;
-  document.querySelector<HTMLInputElement>('#rootDirectory')!.value = extensionConfiguration[ConfigurationKey.ROOT_DIRECTORY];
+
+  rootDirectory!.value = extensionConfiguration[ConfigurationKey.ROOT_DIRECTORY];
+  setRootDirectoryButton?.addEventListener('click', async (_event: HTMLElementEventMap['click']) => {
+    const newRootDirectory = rootDirectory!.value.trim();
+    const currentRootDirectory = await configuration.getKey(ConfigurationKey.ROOT_DIRECTORY);
+
+    if (newRootDirectory === currentRootDirectory) {
+      console.debug('Skipping on setting new root directory as it matches current configuration root directory');
+      return;
+    }
+
+    await configuration.setKey(ConfigurationKey.ROOT_DIRECTORY, newRootDirectory);
+
+    console.debug(`Set configuration root directory to ${newRootDirectory}`);
+  });
 
   document.getElementById('bookUp')?.addEventListener('click', () => {
     browser.runtime.sendMessage({command: 'bookUp'});
